1d5713f96e6d63bc15b08f10907c071433c48c19
Eric Wayne Vaandering <sha1-87df1b4d94f961dadccf9690dda6dfaf3f30ca0c@cern.ch>
2007-03-07 17:08:00 +0000

    Get rid of old, broken test code

--- test/SealModule.cc
+++ test/SealModule.cc
@@ -1,6 +1,5 @@
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "SimDataFormats/TrackingAnalysis/test/MCAccessTest.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE( MCAccessTest );
+DEFINE_ANOTHER_FWK_MODULE( );
